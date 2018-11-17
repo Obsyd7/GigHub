6f4f3a210060b04c8ff1cfccb50bf45239ccd972
Patryk Pater <paterp989@gmail.com>
2018-11-17 19:07:22 +0100

    implement my followings functionality

--- GigHub/Controllers/FolloweesController.cs
+++ GigHub/Controllers/FolloweesController.cs
@@ -1,9 +1,29 @@
-﻿using System.Web.Http;
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
-    [Authorize]
-    public class FolloweesController : ApiController
+    public class FolloweesController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var myFollows = _context.Followings
+                .Where(a => a.FollowerId == userId)
+                .Select(c => c.Followee)
+                .ToList();
+
+            return View(myFollows);
+        }
     }
 }
