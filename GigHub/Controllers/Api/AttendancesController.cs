0c53e0a78fa999ea8baa26bcb103ba542b9ef95d
Patryk Pater <paterp989@gmail.com>
2018-11-24 17:42:17 +0100

    The rest of use cases

--- GigHub/Controllers/Api/AttendancesController.cs
+++ GigHub/Controllers/Api/AttendancesController.cs
@@ -1,8 +1,8 @@
-﻿using System.Linq;
-using System.Web.Http;
-using GigHub.Dtos;
+﻿using GigHub.Dtos;
 using GigHub.Models;
 using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
 
 namespace GigHub.Controllers.Api
 {
@@ -33,6 +33,25 @@ namespace GigHub.Controllers.Api
             _context.Attendances.Add(attendance);
             _context.SaveChanges();
             return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteAttendance(int id)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var attendance = _context.Attendances
+                .SingleOrDefault(a => a.GigId == id && a.AttendeeId == userId);
+
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            _context.Attendances.Remove(attendance);
+            _context.SaveChanges();
+
+            return Ok(id);
         }
     }
 }
