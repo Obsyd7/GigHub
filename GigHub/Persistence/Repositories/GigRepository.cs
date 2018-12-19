e4a014f0b0714cbeb959249edbae83cfc94ef0df
Patryk Pater <paterp989@gmail.com>
2018-12-19 12:02:51 +0100

    Decoupling home controller

--- GigHub/Persistence/Repositories/GigRepository.cs
+++ GigHub/Persistence/Repositories/GigRepository.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using GigHub.Core.Models;
+using GigHub.Core.Repositories;
+using System;
+using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using GigHub.Core.Models;
-using GigHub.Core.Repositories;
 
 namespace GigHub.Persistence.Repositories
 {
@@ -51,6 +52,24 @@ namespace GigHub.Persistence.Repositories
         public Gig GetGigToEdit(int gigId)
         {
             return _context.Gigs.Single(g => g.Id == gigId);
+        }
+
+        public IEnumerable<Gig> GetAllUpcomingGigs()
+        {
+            return _context.Gigs
+                 .Include(g => g.Artist)
+                 .Include(g => g.Genre)
+                 .Where(g => g.DateTime > DateTime.Now && !g.IsCanceled);
+        }
+
+        public IEnumerable<Gig> GetAllUpcomingGigsQuery(string query, IEnumerable<Gig> upComingGigs)
+        {
+
+            return upComingGigs
+                .Where(g =>
+                    g.Artist.Name.Contains(query) ||
+                    g.Genre.Name.Contains(query) ||
+                    g.Venue.Contains(query));
         }
     }
 }
