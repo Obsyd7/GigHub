80d477f164f9d15448eadeb2f63b0cb4b4686116
Patryk Pater <paterp989@gmail.com>
2018-12-23 10:24:47 +0100

    gig repository test setup

---
+++ GigHub.Tests/Persistance/Repositories/GigRepositoriesTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Entity;
+using GigHub.Core.Models;
+using GigHub.Persistence;
+using GigHub.Persistence.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GigHub.Tests.Persistance.Repositories
+{
+    [TestClass]
+    public class GigRepositoriesTests
+    {
+        private GigRepository _repository;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var mockGigs = new Mock<DbSet<Gig>>();
+
+            var mockContext = new Mock<IApplicationDbContext>();
+            mockContext.SetupGet(c => c.Gigs).Returns(mockGigs.Object);
+
+            _repository = new GigRepository(mockContext.Object);
+        }
+    }
+}
